8aa0734004b3502fca95d2f77d86e62d181a631c
Zoom-Developer <sa-mp12345@yandex.ru>
2024-11-16 21:24:50 +0300

    Increase verify modifier

--- backend/src/domain/user/repository.py
+++ backend/src/domain/user/repository.py
@@ -39,7 +39,7 @@ class UserRepository(BaseRepository[User]):
                 select(Like.target_id).where(Like.user_id == user.id).scalar_subquery()
             ))
         ) \
-        .order_by(func.random() * case((User.verify == True, 1), else_=0.7)) \
+        .order_by(func.random() * case((User.verify == True, 1), else_=0.5)) \
         .limit(1)
         res = await self.session.exec(query)
         return res.first()
