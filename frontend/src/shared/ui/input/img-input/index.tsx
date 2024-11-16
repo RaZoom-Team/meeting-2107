e65751d8fe3e82b9dac74dce80dfd4f2cec2ad85
Ramir Vorobyev <rrramirrr@yandex.ru>
2024-11-16 21:52:16 +0300

    heik alert

--- frontend/src/shared/ui/input/img-input/index.tsx
+++ frontend/src/shared/ui/input/img-input/index.tsx
@@ -10,13 +10,17 @@ export function InputImg({hook, src}: Props) {
     const imgChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.files && e.target.files[0]) {
             //console.log(e.target.files[0])
-            hook(URL.createObjectURL(e.target.files[0]))
+            const file = e.target.files[0]
+            console.log(file)
+            if (file.type != 'image/jpg' && file.type != 'image/jpeg' && file.type != 'image/png') {
+                alert('Выберите изображение не типа HEIC')
+            } else hook(URL.createObjectURL(e.target.files[0]))
             //console.log(URL.createObjectURL(e.target.files[0]))
         }
     }
 
     return <div style={src ? {backgroundImage: `url(${src})`} : {}} className={styles['container-input']}>
         <p style={src ? {opacity: '0'} : {}} className={styles['help']}>Нажмите, чтобы выбрать фото</p>
-        <input className={styles['input']} onChange={imgChange} accept="image/png, image/jpeg, image/heic" type='file'></input>
+        <input className={styles['input']} onChange={imgChange} accept="image/jpg, image/png" type='file'></input>
     </div>
 }
