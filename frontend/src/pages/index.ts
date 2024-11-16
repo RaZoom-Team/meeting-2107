8831a6e39af7ba52311c596f757aeef52085d5fb
Ramir Vorobyev <rrramirrr@yandex.ru>
2024-11-16 22:11:37 +0300

    image alert fix + add ban page

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,6 +1,7 @@
 
 export * from './auth/ui/auth'
 export * from './auth/ui/sub'
+export * from './auth/ui/ban'
 export * from './edit/ui/edit'
 export * from './feed/ui/feed'
 export * from './history/ui/history'
